7ee3036770c7fccb468c2caa11cefbb6670d42cf
Marco Antônio Queiroz <marco.aq7@gmail.com>
2017-05-04 16:56:26 -0300

    update schema DB

--- vendor/cakephp-plugins.php
+++ vendor/cakephp-plugins.php
@@ -2,8 +2,13 @@
 $baseDir = dirname(dirname(__FILE__));
 return [
     'plugins' => [
+        'AuthUser' => $baseDir . '/plugins/AuthUser/',
         'Bake' => $baseDir . '/vendor/cakephp/bake/',
+        'Charts' => $baseDir . '/plugins/Charts/',
         'DebugKit' => $baseDir . '/vendor/cakephp/debug_kit/',
-        'Migrations' => $baseDir . '/vendor/cakephp/migrations/'
+        'Migrations' => $baseDir . '/vendor/cakephp/migrations/',
+        'Prints' => $baseDir . '/plugins/Prints/',
+        'Sys' => $baseDir . '/plugins/Sys/',
+        'Template' => $baseDir . '/plugins/Template/'
     ]
 ];
